048173aeb9732eec0bb6b1583bdd88f6129e1186
Long-Brian-Yang <longyang2000110@gmail.com>
2024-11-08 16:58:42 +0900

    updated

--- get_cif.py
+++ get_cif.py
@@ -2,5 +2,5 @@ from pymatgen.ext.matproj import MPRester
 
 # Initialize the MP Rester
 mpr = MPRester("kzum4sPsW7GCRwtOqgDIr3zhYrfpaguK")
-structure = mpr.get_structure_by_material_id("mp-1192651") # Input the material ID
+structure = mpr.get_structure_by_material_id("mp-19035") # Input the material ID
 structure.to(filename="BaFeO3.cif")
--- get_poscar.py
+++ get_poscar.py
@@ -1,7 +1,7 @@
 from ase.io import read, write
 
 # read your CIF file
-structure = read('Ba8Zr8O24.cif')
+structure = read('Ba8Fe8O24.cif')
 
 # write the POSCAR file
-write('POSCAR1', structure, format='vasp')
+write('POSCAR2', structure, format='vasp')
